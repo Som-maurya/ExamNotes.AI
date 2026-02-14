b675153043c2f28efc1d26ae082f5948f5403587
Somkumar maurya <183973511+Som-maurya@users.noreply.github.com>
2026-02-15 00:35:35 +0530

    Update CORS origin for production environment

--- server/index.js
+++ server/index.js
@@ -26,7 +26,7 @@ app.post(
 );
 
 app.use(cors(
-    {origin: process.env.FRONTEND_URL || "http://localhost:5173",
+    {origin: process.env.FRONTEND_URL || "https://examnotes-aiclient.onrender.com",
         credentials:true,
         methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"]
     }
